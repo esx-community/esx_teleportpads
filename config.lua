1a6c8ca34eddc5c0562ec72f28ad81b57a677fbe
ElPumpo <samo_kol@hotmail.com>
2018-08-16 00:36:57 +0200

    Initial commit

---
+++ config.lua
@@ -0,0 +1,25 @@
+Config              = {}
+
+Config.DrawDistance = 100.0
+
+Config.Marker = {
+	Type = 1,
+	x = 1.5, y = 1.5, z = 1.0,
+	r = 255, g = 55, b = 55
+}
+
+Config.Pads = {
+
+	MartinHouseIn = {
+		Text = 'Press ~INPUT_CONTEXT~ to enter ~y~house~s~.',
+		Marker = { x = 1400.65, y = 1126.64, z = 113.30 },
+		TeleportPoint = { x = 1400.53, y = 1129.06, z = 114.33, h = 0.87 }
+	},
+
+	MartinHouseOut = {
+		Text = 'Press ~INPUT_CONTEXT~ to leave the ~y~house~s~.',
+		Marker = { x = 1400.49, y = 1129.61, z = 113.30 },
+		TeleportPoint = { x = 1400.25, y = 1123.59, z = 114.84, h = 180.0 }
+	}
+
+}
