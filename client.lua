1377af4d36d71de7f7ca4a2b9325ae2a2249bbe5
Søren Eskildsen <soeren@bzn.dk>
2019-03-10 00:52:22 +0100

    Indent Fix

--- client.lua
+++ client.lua
@@ -1,13 +1,13 @@
 local Keys = {
-  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+    ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
 
 local HasAlreadyEnteredMarker = false
@@ -21,102 +21,102 @@ local CurrentActionData = nil
 ESX = nil
 
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj)ESX = obj end)
+        Citizen.Wait(0)
+    end
 end)
 
 RegisterNetEvent('esx_teleportpads:hasEnteredMarker')
 AddEventHandler('esx_teleportpads:hasEnteredMarker', function(currentPad, padData)
-	CurrentAction = 'pad.' .. string.lower(currentPad)
-	CurrentActionMsg = padData.Text
-	CurrentActionData = { padData = padData }
+    CurrentAction = 'pad.' .. string.lower(currentPad)
+    CurrentActionMsg = padData.Text
+    CurrentActionData = {padData = padData}
 end)
 
 RegisterNetEvent('esx_teleportpads:hasExitedMarker')
 AddEventHandler('esx_teleportpads:hasExitedMarker', function()
-	ESX.UI.Menu.CloseAll()
+    ESX.UI.Menu.CloseAll()
 
-	CurrentAction = nil
+    CurrentAction = nil
 end)
 
 -- Draw marker
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		local playerPed = PlayerPedId()
-		local coords = GetEntityCoords(playerPed)
-		local canSleep = true
+    while true do
+        Citizen.Wait(0)
+        local playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed)
+        local canSleep = true
 
-		for pad, padData in ipairs(Config.Pads) do
-			if GetDistanceBetweenCoords(coords, padData.Marker, true) < Config.DrawDistance then
-				DrawMarker(padData.MarkerSettings.type, padData.Marker, 0.0, 0.0, 0.0, 0, 0.0, 0.0, padData.MarkerSettings.x, padData.MarkerSettings.y, padData.MarkerSettings.z, padData.MarkerSettings.r, padData.MarkerSettings.g, padData.MarkerSettings.b, padData.MarkerSettings.a, false, true, 2, false, false, false, false)
-				canSleep = false
-			end
+        for pad, padData in ipairs(Config.Pads) do
+            if GetDistanceBetweenCoords(coords, padData.Marker, true) < Config.DrawDistance then
+                DrawMarker(padData.MarkerSettings.type, padData.Marker, 0.0, 0.0, 0.0, 0, 0.0, 0.0, padData.MarkerSettings.x, padData.MarkerSettings.y, padData.MarkerSettings.z, padData.MarkerSettings.r, padData.MarkerSettings.g, padData.MarkerSettings.b, padData.MarkerSettings.a, false, true, 2, false, false, false, false)
+                canSleep = false
+            end
 
-			if canSleep then
-				Citizen.Wait(500)
-			end
-		end
-	end
+            if canSleep then
+                Citizen.Wait(500)
+            end
+        end
+    end
 end)
 
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		
-		local playerPed = PlayerPedId()
-		local coords, isInMarker, currentPad, currentAction, currentPadData = GetEntityCoords(playerPed), false, nil, nil, nil
+    while true do
+        Citizen.Wait(0)
 
-		for pad, padData in ipairs(Config.Pads) do
-			if GetDistanceBetweenCoords(coords, padData.Marker, true) < padData.MarkerSettings.x then
-				isInMarker, currentPad, currentAction, currentPadData = true, pad, 'pad.' .. string.lower(pad), padData
-			end
-		end
+        local playerPed = PlayerPedId()
+        local coords, isInMarker, currentPad, currentAction, currentPadData = GetEntityCoords(playerPed), false, nil, nil, nil
 
-		local hasExited = false
+        for pad, padData in ipairs(Config.Pads) do
+            if GetDistanceBetweenCoords(coords, padData.Marker, true) < padData.MarkerSettings.x then
+                isInMarker, currentPad, currentAction, currentPadData = true, pad, 'pad.' .. string.lower(pad), padData
+            end
+        end
 
-		if isInMarker and not HasAlreadyEnteredMarker or (isInMarker and (LastPad ~= currentPad or LastAction ~= currentAction)) then
-			if (LastPad ~= nil and LastAction ~= nil) and (LastPad ~= currentPad or LastAction ~= currentAction) then
-				TriggerEvent('esx_teleportpads:hasExitedMarker', LastPad, LastAction)
-				
-				hasExited = true
-			end
+        local hasExited = false
 
-			HasAlreadyEnteredMarker = true
-			LastPad, LastAction, LastPadData = currentPad, currentAction, currentPadData
+        if isInMarker and not HasAlreadyEnteredMarker or (isInMarker and (LastPad ~= currentPad or LastAction ~= currentAction)) then
+            if (LastPad ~= nil and LastAction ~= nil) and (LastPad ~= currentPad or LastAction ~= currentAction) then
+                TriggerEvent('esx_teleportpads:hasExitedMarker', LastPad, LastAction)
 
-			TriggerEvent('esx_teleportpads:hasEnteredMarker', currentPad, currentPadData)
-		end
+                hasExited = true
+            end
 
-		if not hasExited and not isInMarker and HasAlreadyEnteredMarker then
-			HasAlreadyEnteredMarker = false
+            HasAlreadyEnteredMarker = true
+            LastPad, LastAction, LastPadData = currentPad, currentAction, currentPadData
 
-			TriggerEvent('esx_teleportpads:hasExitedMarker', LastPad, LastAction)
-		end
+            TriggerEvent('esx_teleportpads:hasEnteredMarker', currentPad, currentPadData)
+        end
 
-		if not HasAlreadyEnteredMarker then
+        if not hasExited and not isInMarker and HasAlreadyEnteredMarker then
+            HasAlreadyEnteredMarker = false
+
+            TriggerEvent('esx_teleportpads:hasExitedMarker', LastPad, LastAction)
+        end
+
+        if not HasAlreadyEnteredMarker then
             Citizen.Wait(500)
         end
-	end
+    end
 end)
 
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
+    while true do
+        Citizen.Wait(0)
 
-		if CurrentAction ~= nil then
+        if CurrentAction ~= nil then
 
-			ESX.ShowHelpNotification(CurrentActionMsg)
+            ESX.ShowHelpNotification(CurrentActionMsg)
 
-			if IsControlJustReleased(0, Keys['E']) then
-				ESX.Game.Teleport(PlayerPedId(), CurrentActionData.padData.TeleportPoint.coords, function()
-					SetEntityHeading(PlayerPedId(), CurrentActionData.padData.TeleportPoint.h)
-				end)
-			end
-		else
-			Citizen.Wait(500)
-		end
-	end
+            if IsControlJustReleased(0, Keys['E']) then
+                ESX.Game.Teleport(PlayerPedId(), CurrentActionData.padData.TeleportPoint.coords, function()
+                    SetEntityHeading(PlayerPedId(), CurrentActionData.padData.TeleportPoint.h)
+                end)
+            end
+        else
+            Citizen.Wait(500)
+        end
+    end
 end)
--- config.lua
+++ config.lua
@@ -4,18 +4,18 @@ Config.DrawDistance = 50.0
 
 Config.Pads = {
 
-	{
-		Text = 'Press ~INPUT_CONTEXT~ to enter ~y~house~s~.',
-		Marker = vector3(1400.6, 1126.6, 113.3),
-		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 1, x = 1.5, y = 1.5, z = 0.5},
-		TeleportPoint = { coords = vector3(1400.5, 1129.0, 114.3), h = 0.87 }
-	},
+        {
+            Text = 'Press ~INPUT_CONTEXT~ to enter ~y~house~s~.',
+            Marker = vector3(1400.6, 1126.6, 113.3),
+            MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 1, x = 1.5, y = 1.5, z = 0.5},
+            TeleportPoint = {coords = vector3(1400.5, 1129.0, 114.3), h = 0.87}
+        },
 
-	{
-		Text = 'Press ~INPUT_CONTEXT~ to leave the ~y~house~s~.',
-		Marker = vector3(1400.4, 1129.6, 113.3),
-		MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 1, x = 1.5, y = 1.5, z = 0.5},
-		TeleportPoint = { coords = vector3(1400.2, 1123.5, 114.8), h = 180.0 }
-	}
+        {
+            Text = 'Press ~INPUT_CONTEXT~ to leave the ~y~house~s~.',
+            Marker = vector3(1400.4, 1129.6, 113.3),
+            MarkerSettings = {r = 255, g = 55, b = 55, a = 100, type = 1, x = 1.5, y = 1.5, z = 0.5},
+            TeleportPoint = {coords = vector3(1400.2, 1123.5, 114.8), h = 180.0}
+        }
 
 }
